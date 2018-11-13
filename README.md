c536e15f61eb342db3ae99a36c4e30f31d43b267
katerina-tziala <katerina.tziala@gmail.com>
2018-11-13 08:19:07 +0200

    del

--- README.md
+++
@@ -1,2 +0,0 @@
-# budget_manager_ems_app
-Budget Manager - Expense Monitoring System
--- src/assets/libs/Timepicker-master/src/js/index.js
+++
@@ -1,8 +0,0 @@
-import formatTime from "./timeFormatter";
-import showPicker from "./timepickerCreator";
-
-export default {
-    showPicker: (config) => showPicker(config),
-    format: (time) => formatTime(time)
-};
-
